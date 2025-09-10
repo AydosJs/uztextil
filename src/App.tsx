20c425ee62528a4b6b97f8611e300a17646378fd
a.sankibaev <aydos.dev@gmail.com>
2025-09-10 17:53:38 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { useTelegramBackButton } from "@/lib/hooks"
 // import { TelegramUserDebug } from "@/components/TelegramUserDebug"
 import { useTelegramUser } from "@/hooks/useTelegramUser"
 import { SliderCards } from "@/components/SliderCards"
-import { showToast } from "@/lib/utils"
 // import { DeviceTest } from "@/components/DeviceTest"
 
 function App() {
@@ -25,10 +24,6 @@ function App() {
         }
     }
 
-    const handleTestToast = () => {
-        showToast.success("Test toast notification!")
-    }
-
     return (
         <div className="min-h-screen min-w-full safe-area-pt w-full dark flex flex-col relative">
 
@@ -42,14 +37,6 @@ function App() {
             </main>
 
             <div className="fixed bottom-0 left-0 right-0 px-4 pt-2 pb-8 safe-area-pb bg-gradient-to-t from-black to-dark z-50 flex flex-col gap-2">
-                <Button
-                    className="max-w-[400px] mx-auto"
-                    variant="outline"
-                    shadow="lg"
-                    onClick={handleTestToast}
-                >
-                    Test Toaster
-                </Button>
                 <Button
                     className="max-w-[400px] mx-auto"
                     variant="default"
